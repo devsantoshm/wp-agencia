47238d0b19a83bd425e397e82933a404def7ae2c
SantosHM <dosantoshm@gmail.com>
2019-03-11 13:10:51 -0500

    Dando estilos a single-tours y a la galeria

--- functions.php
+++ functions.php
@@ -28,7 +28,8 @@ if (function_exists('add_theme_support'))
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
     add_image_size('mediano', 350, 250, true);
-    add_image_size('toursDestacado', 523, 294, true); 
+    add_image_size('toursDestacado', 523, 294, true);
+    add_image_size('toursPrincipal', 751, 422, true); 
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     /*add_theme_support('custom-background', array(
