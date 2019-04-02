91cb3b927e5db7bbd5951a60faef8a05fe29c872
SantosHM <dosantoshm@gmail.com>
2019-04-02 16:10:37 -0500

    dando estilos css al login de wp

--- login/js/login.js
+++ login/js/login.js
@@ -1,3 +1,13 @@
 jQuery(function($){
-	console.log(login_imagenes.ruta_plantilla)
+	//console.log(login_imagenes.ruta_plantilla)
+	$('#login h1, #login form').wrapAll('<div class="grupo"></div>')
+	$('body').vegas({
+		slides:[
+			{src: login_imagenes.ruta_plantilla + "/login/img/1.jpg"},
+			{src: login_imagenes.ruta_plantilla + "/login/img/2.jpg"},
+			{src: login_imagenes.ruta_plantilla + "/login/img/3.jpg"}
+		],
+		overlay: login_imagenes.ruta_plantilla + "/login/img/overlay/05.png",
+		transition: ['fade', 'zoomOut', 'swirlLeft']
+	})
 })
